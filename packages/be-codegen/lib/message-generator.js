5516e44a8364dabe542ad09e88f433b0cfe1a16f
Mantas Indrašius <mantasi@wix.com>
2019-05-19 18:19:07 +0300

    Add message generator

--- packages/be-codegen/lib/message-generator.js
+++ packages/be-codegen/lib/message-generator.js
@@ -1,37 +1,99 @@
+const protobuf = require('protobufjs');
 
 module.exports = {
-  generateMessageUnit(messageType) {
-    const jsRefs = {};
-    const {jsFields} = formatMessageFields(messageType.fields, jsRefs);
-
-    const fnCode = `
-      function define() {
-        const type = ${reference('MessageBuilder', 'be-proto:runtime', jsRefs)}.create()
-          ${jsFields};
-      }
-    `;
-
-    return {
-      js: {
-        refs: jsRefs,
-        code: fnCode
-      },
-      ts: {
-        refs: [],
-        code: ``
-      }
-    };
-  }
+  generateMessageUnit,
+  generateEnum
 };
 
-function formatMessageFields(fields, jsRefs) {
+function generateMessageUnit(messageType) {
+  const jsRefs = {};
+  const {jsFields} = formatMessageFields(messageType, jsRefs);
+
+  const fnCode = `${reference('MessageBuilder', 'be-proto:runtime', jsRefs)}.create()
+        ${jsFields};`;
+
+  return {
+    name: messageType.name,
+    namespace: resolveNamespace(messageType).join('.'),
+    nested: collectNestedTypes(messageType),
+    messageType,
+    js: {
+      refs: jsRefs,
+      code: fnCode
+    },
+    ts: {
+      refs: [],
+      code: ``
+    }
+  };
+}
+
+function generateEnum(enumType) {
+  const jsRefs = {};
+  const {jsFields} = formatEnumFields(enumType.values);
+
+  const fnCode = `${reference('EnumBuilder', 'be-proto:runtime', jsRefs)}.create()
+        ${jsFields};`;
+
+  return {
+    name: enumType.name,
+    namespace: resolveNamespace(enumType).join('.'),
+    enumType,
+    js: {
+      refs: jsRefs,
+      code: fnCode
+    },
+    ts: {
+      refs: [],
+      code: ``
+    }
+  };
+}
+
+function collectNestedTypes(node) {
+  if (!node.nested) {
+    return undefined;
+  }
+
+  const nested = {};
+
+  Object.keys(node.nested).forEach((name) => {
+    const child = node.nested[name];
+
+    if (child instanceof protobuf.Type) {
+      nested[name] = generateMessageUnit(child);
+    } else if (child instanceof protobuf.Enum) {
+      nested[name] = generateEnum(child);
+    }
+  });
+
+  return nested;
+}
+
+function formatMessageFields(messageType, jsRefs) {
   const jsFields = [];
 
-  Object.values(fields).forEach((field) => {
+  Object.values(messageType.fields).forEach((field) => {
     const fieldMethod = field.repeated ? 'repeated' : 'field';
     const fieldModifier = field.partOf ? `${reference('oneOf', 'be-proto:runtime', jsRefs)}('${field.partOf.name}', ${field.id})` : field.id;
 
-    jsFields.push(`.${fieldMethod}('${field.name}', ${reference(field.type, 'be-proto:runtime', jsRefs)}, ${fieldModifier})`);
+    const source = messageType;
+
+    jsFields.push(`.${fieldMethod}('${field.name}', ${reference(field.type, source, jsRefs)}, ${fieldModifier})`);
+  });
+
+  return {
+    jsFields: jsFields.join('\r\n')
+  };
+}
+
+function formatEnumFields(values) {
+  const jsFields = [];
+
+  Object.keys(values).forEach((name) => {
+    const value = typeof(values[name]) === 'string' ? `'${values[name]}'` : values[name];
+
+    jsFields.push(`.value('${name}', ${value})`);
   });
 
   return {
@@ -46,4 +108,18 @@ function reference(id, source, refs) {
   };
 
   return id;
+}
+
+function resolveNamespace(node, namespace = []) {
+  const parent = node.parent;
+
+  if (parent) {
+    if (parent instanceof protobuf.Namespace && parent.name > '') {
+      namespace.unshift(parent.name);
+    }
+
+    resolveNamespace(parent, namespace);
+  }
+
+  return namespace;
 }
--- packages/be-codegen/lib/message-generator.spec.js
+++ packages/be-codegen/lib/message-generator.spec.js
@@ -1,4 +1,4 @@
-const {generateMessageUnit} = require('./message-generator');
+const {generateMessageUnit, generateEnum} = require('./message-generator');
 const protobuf = require('protobufjs');
 const {expect} = require('chai');
 const _ = require('lodash');
@@ -9,14 +9,19 @@ describe('message-generator', () => {
     const givenProto = protobuf.parse(`
       syntax = "proto3";
 
+      package a.test;
+
       message TestMessage {
         string test_value = 1;
         repeated int64 test_values = 2;
       }
     `);
 
-    const generatedMessage = generateMessageUnit(givenProto.root.TestMessage);
+    const generatedMessage = generateMessageUnit(givenProto.root.nested.a.nested.test.TestMessage);
 
+    expect(generatedMessage.js.code).to.include(`MessageBuilder`);
+    expect(generatedMessage.namespace).to.equal('a.test');
+    expect(generatedMessage.name).to.equal('TestMessage');
     expect(generatedMessage.js.code).to.include(`.field('testValue', string, 1)`);
     expect(generatedMessage.js.code).to.include(`.repeated('testValues', int64, 2)`);
 
@@ -44,5 +49,80 @@ describe('message-generator', () => {
     expect(generatedMessage.js.code).to.include(`.field('testValue', string, oneOf('value', 1)`);
     expect(generatedMessage.js.code).to.include(`.field('testIntValue', int32, oneOf('value', 2))`);
     expect(generatedMessage.js.refs.oneOf).to.exist;
+  });
+
+  it('should reference another message', () => {
+    const givenProto = protobuf.parse(`
+      syntax = "proto3";
+
+      message TestMessage {
+        TestMessage2 msg = 1;
+      }
+
+      message TestMessage2 {
+        TestMessage msg = 1;
+      }
+    `);
+
+    const generatedMessage = generateMessageUnit(givenProto.root.TestMessage);
+
+    expect(generatedMessage.js.code).to.include(`MessageBuilder`);
+    expect(generatedMessage.js.code).to.include(`.field('msg', TestMessage2, 1)`);
+
+    expect(generatedMessage.js.refs.TestMessage2).to.deep.equal({
+      id: 'TestMessage2',
+      source: givenProto.root.TestMessage
+    });
+    expect(generatedMessage.js.refs.MessageBuilder).to.deep.equal({
+      source: 'be-proto:runtime',
+      id: 'MessageBuilder'
+    });
+  });
+
+  it('should generate int enum', () => {
+    const givenProto = protobuf.parse(`
+      syntax = "proto3";
+
+      enum TestEnum {
+        A = 0;
+        B = 1;
+      }
+    `);
+
+    const generatedMessage = generateEnum(givenProto.root.nested.TestEnum);
+
+    expect(generatedMessage.js.code).to.include(`EnumBuilder`);
+    expect(generatedMessage.js.code).to.include(`.value('A', 0)`);
+    expect(generatedMessage.js.code).to.include(`.value('B', 1)`);
+  });
+
+  it('should generate nested objects', () => {
+    const givenProto = protobuf.parse(`
+      syntax = "proto3";
+
+      message TestMessage {
+        message TestNestedMessage {
+          string id = 1;
+        }
+
+        enum TestEnum {
+          A = 0;
+          B = 1;
+        }
+
+        TestNestedMessage msg = 1;
+        TestEnum enm = 2;
+      }
+    `);
+
+    const generatedMessage = generateMessageUnit(givenProto.root.nested.TestMessage);
+
+    expect(generatedMessage.nested.TestNestedMessage.js.code).to.include(`MessageBuilder`);
+    expect(generatedMessage.nested.TestEnum.js.code).to.include(`EnumBuilder`);
+    expect(generatedMessage.js.refs.TestNestedMessage).to.deep.equal({
+      id: 'TestNestedMessage',
+      source: givenProto.root.nested.TestMessage
+    });
+    expect(generatedMessage.js.refs.TestEnum).to.exist;
   });
 });
