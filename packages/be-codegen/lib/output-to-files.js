9f67c3d22048355f51aa6f26ff77acc177d4e91a
Mantas Indrašius <mantasi@wix.com>
2019-05-25 01:03:02 +0300

    Missing file

---
+++ packages/be-codegen/lib/output-to-files.js
@@ -0,0 +1,32 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports = function outputToFiles(outputDir, output = console) {
+
+  let fulfillCallback;
+
+  const pendingPromises = [];
+  const completePromise = new Promise((fulfill, reject) => {
+    fulfillCallback = fulfill;
+  })
+
+  return {
+    add(code) {
+      const relativeBasename = code.namespace ? `${code.namespace}/${code.name}` : `${code.name}`;
+      const jsName = `${relativeBasename}.js`;
+      const filepath = path.join(outputDir, jsName);
+
+      const promise = fs.outputFile(filepath, code.js.code).then(() => output.log(`== Generated ${relativeBasename}`));
+
+      pendingPromises.push(promise);
+    },
+    complete() {
+      fulfillCallback();
+    },
+    async done() {
+      await fulfillCallback();
+      await Promise.all(pendingPromises);
+      output.log('== Complete');
+    }
+  }
+};
