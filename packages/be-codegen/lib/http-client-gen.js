1c868e5d363e8fe8e871803a1adfc684f230c314
Mantas Indrašius <mantasi@wix.com>
2019-05-28 16:36:52 +0300

    Primitive types

--- packages/be-codegen/lib/http-client-gen.js
+++ packages/be-codegen/lib/http-client-gen.js
@@ -128,7 +128,21 @@ function formatTypeName(descriptor) {
 }
 
 const builtinTypes = {
-  string: {}
+  double: {},
+  float: {},
+  int32: {},
+  int64: {},
+  uint32: {},
+  uint64: {},
+  sint32: {},
+  sint64: {},
+  fixed32: {},
+  fixed64: {},
+  sfixed32: {},
+  sfixed64: {},
+  bool: {},
+  string: {},
+  bytes: {}
 };
 
 module.exports = {httpClientGen, mapImport, mapLocalImport};
